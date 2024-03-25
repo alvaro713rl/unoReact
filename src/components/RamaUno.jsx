afebbf9f7541e46611d984464958be9049ea0cb5
Alvaro713rl <aareyeslinares@gmail.com>
2024-03-25 12:02:10 -0400

    primer commit

---
+++ src/components/RamaUno.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RamaUno = () => {
+  return (
+    <div>
+      <h1>Hola desde react</h1>
+      <h2>Saludos desde la rama Maaain</h2>
+      <h2>creado desde windows</h2> 
+    </div>
+  )
+}
+
+export default RamaUno
