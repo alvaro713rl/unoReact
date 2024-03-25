631abedb0abdf62bdd40903fcd7546cacd32ce8e
Alvaro713rl <aareyeslinares@gmail.com>
2024-03-25 12:03:15 -0400

    segundo commit

--- src/components/RamaUno.jsx
+++ src/components/RamaUno.jsx
@@ -6,6 +6,7 @@ const RamaUno = () => {
       <h1>Hola desde react</h1>
       <h2>Saludos desde la rama Maaain</h2>
       <h2>creado desde windows</h2> 
+      <h3>para el  segundo comit</h3>
     </div>
   )
 }
